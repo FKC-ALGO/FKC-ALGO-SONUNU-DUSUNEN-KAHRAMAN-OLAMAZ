20891781ab097b1f1612512e270af75ab8264db6
BartuKaptanoglu <139239962+BartuKaptanoglu@users.noreply.github.com>
2023-11-01 18:08:52 +0300

    Update SONUNUDÖŞÜNENKAHRAMANOLMAZ.cpp

--- SONUNUDÖŞÜNENKAHRAMANOLMAZ.cpp
+++ SONUNUDÖŞÜNENKAHRAMANOLMAZ.cpp
@@ -1,21 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int partition(int arr[],int low,int high)
 {
-//choose the pivot
 
 int pivot=arr[high];
-//Index of smaller element and Indicate
-//the right position of pivot found so far
+
 int i=(low-1);
 
 for(int j=low;j<=high;j++)
 {
-	//If current element is smaller than the pivot
 	if(arr[j]<pivot)
 	{
-	//Increment index of smaller element
+
 	i++;
 	swap(arr[i],arr[j]);
 	}
@@ -24,20 +21,14 @@ swap(arr[i+1],arr[high]);
 return (i+1);
 }
 
-// The Quicksort function Implement
+
 			
 void quickSort(int arr[],int low,int high)
 {
-// when low is less than high
 if(low<high)
 {
-	// pi is the partition return index of pivot
 	
 	int pi=partition(arr,low,high);
-	
-	//Recursion Call
-	//smaller element than pivot goes left and
-	//higher element goes right
 	quickSort(arr,low,pi-1);
 	quickSort(arr,pi+1,high);
 }
@@ -57,4 +48,3 @@ for(int i=0;i<n;i++)
 }
 return 0;
 }
-// This Code is Contributed By Diwakar Jha
