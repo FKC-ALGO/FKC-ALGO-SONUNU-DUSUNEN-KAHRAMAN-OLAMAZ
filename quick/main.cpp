d8916e595710bdf608e382b5fdf5b99fa19b1c12
Ruzgar Imren <135638786+ruzgarimren@users.noreply.github.com>
2023-11-07 20:10:04 +0300

    Update main.cpp

--- quick/main.cpp
+++ quick/main.cpp
@@ -20,11 +20,11 @@ int partition(int arr[],int low,int high)
 		if (arr[j] < pivot)
 		{
 			i++;
-			swap(arr[i],arr[j]);
+			swap(arr[i], arr[j]);
 		}
 	}
 
-	swap(arr[i+1],arr[high]);
+	swap(arr[i+1], arr[high]);
 	return (i+1);
 }
 
@@ -45,7 +45,7 @@ int main() {
 	int arr[] = {10,7,8,9,1,5};
 	int n = sizeof(arr) / sizeof(arr[0]);
 
-	quickSort(arr,0,n-1);
+	quickSort(arr, 0, n-1);
 
 	cout << "Sorted Array\n";
 	for (int i = 0; i < n; i++)
